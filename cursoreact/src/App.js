66ff32c481e958a2d79bd78c7088fba56c01341a
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-03-07 10:48:40 -0300

    criei um componente e importei ele

---
+++ cursoreact/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import FirstComponent from './components/firstComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
